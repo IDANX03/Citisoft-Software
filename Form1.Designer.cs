c59500715bb80a194ea51346df5921712f7601b2
Idan X <iamidanx@gmail.com>
2023-11-28 19:54:50 +0000

    login page

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,19 +31,19 @@
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
-            label4 = new Label();
-            confirm_password_textbox = new TextBox();
+            email_textbox = new TextBox();
             password_textbox = new TextBox();
-            username_textbox = new TextBox();
             show_password_checkbox = new CheckBox();
-            btn_register = new Button();
+            btn_login = new Button();
+            label4 = new Label();
+            label5 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Poor Richard", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(135, 60);
+            label1.Location = new Point(138, 29);
             label1.Name = "label1";
             label1.Size = new Size(176, 38);
             label1.TabIndex = 0;
@@ -56,9 +56,9 @@
             label2.Font = new Font("Rockwell", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label2.Location = new Point(22, 143);
             label2.Name = "label2";
-            label2.Size = new Size(108, 22);
+            label2.Size = new Size(151, 22);
             label2.TabIndex = 1;
-            label2.Text = "Username:";
+            label2.Text = "Email Address:";
             // 
             // label3
             // 
@@ -70,40 +70,21 @@
             label3.TabIndex = 2;
             label3.Text = "Password:";
             // 
-            // label4
+            // email_textbox
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Rockwell", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.Location = new Point(22, 285);
-            label4.Name = "label4";
-            label4.Size = new Size(182, 22);
-            label4.TabIndex = 3;
-            label4.Text = "Confirm Password:";
-            // 
-            // confirm_password_textbox
-            // 
-            confirm_password_textbox.BorderStyle = BorderStyle.FixedSingle;
-            confirm_password_textbox.ForeColor = SystemColors.WindowText;
-            confirm_password_textbox.Location = new Point(219, 285);
-            confirm_password_textbox.Name = "confirm_password_textbox";
-            confirm_password_textbox.Size = new Size(243, 27);
-            confirm_password_textbox.TabIndex = 4;
+            email_textbox.BorderStyle = BorderStyle.FixedSingle;
+            email_textbox.Location = new Point(219, 138);
+            email_textbox.Name = "email_textbox";
+            email_textbox.Size = new Size(243, 27);
+            email_textbox.TabIndex = 5;
             // 
             // password_textbox
             // 
             password_textbox.BorderStyle = BorderStyle.FixedSingle;
-            password_textbox.Location = new Point(219, 211);
+            password_textbox.Location = new Point(219, 206);
             password_textbox.Name = "password_textbox";
             password_textbox.Size = new Size(243, 27);
-            password_textbox.TabIndex = 5;
-            // 
-            // username_textbox
-            // 
-            username_textbox.BorderStyle = BorderStyle.FixedSingle;
-            username_textbox.Location = new Point(219, 143);
-            username_textbox.Name = "username_textbox";
-            username_textbox.Size = new Size(243, 27);
-            username_textbox.TabIndex = 6;
+            password_textbox.TabIndex = 6;
             // 
             // show_password_checkbox
             // 
@@ -111,34 +92,56 @@
             show_password_checkbox.Cursor = Cursors.Hand;
             show_password_checkbox.FlatStyle = FlatStyle.Flat;
             show_password_checkbox.Font = new Font("Times New Roman", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            show_password_checkbox.Location = new Point(366, 334);
+            show_password_checkbox.Location = new Point(361, 257);
             show_password_checkbox.Name = "show_password_checkbox";
             show_password_checkbox.Size = new Size(121, 21);
             show_password_checkbox.TabIndex = 7;
             show_password_checkbox.Text = "Show Password";
             show_password_checkbox.UseVisualStyleBackColor = true;
             // 
-            // btn_register
+            // btn_login
             // 
-            btn_register.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btn_register.Location = new Point(188, 357);
-            btn_register.Name = "btn_register";
-            btn_register.Size = new Size(152, 42);
-            btn_register.TabIndex = 8;
-            btn_register.Text = "REGISTER";
-            btn_register.UseVisualStyleBackColor = true;
+            btn_login.Cursor = Cursors.Hand;
+            btn_login.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_login.Location = new Point(150, 297);
+            btn_login.Name = "btn_login";
+            btn_login.Size = new Size(152, 42);
+            btn_login.TabIndex = 8;
+            btn_login.Text = "LOGIN";
+            btn_login.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(138, 384);
+            label4.Name = "label4";
+            label4.Size = new Size(181, 26);
+            label4.TabIndex = 9;
+            label4.Text = "Need an Account?";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Cursor = Cursors.Hand;
+            label5.Font = new Font("Tahoma", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(162, 423);
+            label5.Name = "label5";
+            label5.Size = new Size(140, 21);
+            label5.TabIndex = 10;
+            label5.Text = "Create Account";
             // 
             // login_form
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(494, 486);
-            Controls.Add(btn_register);
-            Controls.Add(show_password_checkbox);
-            Controls.Add(username_textbox);
-            Controls.Add(password_textbox);
-            Controls.Add(confirm_password_textbox);
+            Controls.Add(label5);
             Controls.Add(label4);
+            Controls.Add(btn_login);
+            Controls.Add(show_password_checkbox);
+            Controls.Add(password_textbox);
+            Controls.Add(email_textbox);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -154,11 +157,14 @@
         private Label label1;
         private Label label2;
         private Label label3;
-        private Label label4;
         private TextBox confirm_password_textbox;
+        private TextBox email_textbox;
         private TextBox password_textbox;
-        private TextBox username_textbox;
         private CheckBox show_password_checkbox;
-        private Button btn_register;
+        private Button btn_login;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox full_name_textbox;
     }
 }
